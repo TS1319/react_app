bde7a26e7d888986cf746ca1df7ee026581ed4ec
TS1319 <80575421+TS1319@users.noreply.github.com>
2024-03-31 17:13:21 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import Header from './components/Headers';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src='https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/6e61b/MainAfter.avif' 
+      alt="frog"/>
+          
+   <h1>Welcome To That Frog !!!!</h1>
+    </div>
+  );
+}
+
+export default App;
